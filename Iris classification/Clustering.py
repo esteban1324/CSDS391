1cabae67995b7cffd69b32089bf24c0af3b6a5a6
esteban1324 <118007706+esteban1324@users.noreply.github.com>
2023-11-11 16:10:11 -0500

    kmeans 11/11

--- Iris classification/Clustering.py
+++ Iris classification/Clustering.py
@@ -6,7 +6,7 @@ import math
 import csv 
 import pandas as pd
 
-""" This is the main class for K means clustering for all the data points. """
+""" This is the main class for K means clustering for all the data points. This is for Question 1. (a-d) """
 class K_Means():
     
     def __init__(self, k:int, data:np.ndarray, max_iter:int):
@@ -14,8 +14,8 @@ class K_Means():
         self.data = data
         self.max_iter = max_iter
         self.centroids = self.initialize_centroids()
-        self.cluster_assignment = np.zeros(len(self.data))
-        
+        self.inertia = []
+             
     # initialize the centroids within range of random data points k times
     def initialize_centroids(self)-> np.ndarray:   
         random.seed(32)
@@ -25,61 +25,76 @@ class K_Means():
         centroids = self.data[indices]
                 
         return centroids
-    
-    
-    
-    
-        
-    # Calculate the sum of squared distances between each data point and its nearest centroid.   
-    def objective_function(self) -> float:
-        total_distance = 0 
-            
-        # Summation of k centroids and data points to find the total distance
-        for i in range(len(self.data)):     
-            min_distance = math.inf     
-            distance = np.linalg.norm(self.data[i, 2:4] - self.centroids[:, 2:4])
-            # update the minimum distance
-            min_distance = np.min(distance)  
-             
-        
-            total_distance += min_distance ** 2
-            
+         
+    def objective_function(self, cluster_assignment:np.ndarray, centroids:np.ndarray, data: np.ndarray) -> float:
+        total_distance = 0
+
+   # For each data point, calculate the distance to its assigned centroid
+        for i in range(data.shape[0]):
+            k = cluster_assignment[i]
+            distance = np.linalg.norm((data[i, 2:4] - centroids[k, 2:4]) ** 2, axis = 0)
+       
+            # Add the squared distance to the total distance
+            total_distance += distance 
+
         return total_distance
+
     
-    
-    # kmeans algorithm, for 11/11 put implement helper functions to make it easier to read and get correct results for elbow method
-    def kmeans(self):
+    # assign the clusters to the data points, based on the closest centroid
+    def assign_clusters(self, centroids:np.ndarray, data: np.ndarray) -> np.ndarray:
         
+        cluster_assignment = np.zeros((data.shape[0], self.k), float)
+        
+        for k in range(self.k):
+            distance = np.linalg.norm(data[:, 2:4] - centroids[k, 2:4], axis = 1)
+            cluster_assignment[:, k] = distance 
+                 
+        cluster = np.argmin(cluster_assignment, axis = 1)
+        
+        return cluster
+    
+    # update the centroids
+    def update_centroids(self, cluster_assignment:np.ndarray, data:np.ndarray) -> np.ndarray:
+            # update the centroids by taking the mean of the data points in each cluster
+            updated_centroids = np.zeros((self.k, data.shape[1]), float)
+            # take the mean of the data points in each cluster, only where the cluster assignment is equal to the centroid index to calc mean.
+            for k in range(self.k):
+                indices = np.where(cluster_assignment == k)
+                selected_data = data[indices]
+                updated_centroids[k, :] = np.mean(selected_data, axis = 0)
+                
+                
+            return updated_centroids
+        
+    # kmeans algorithm, which is the main function
+    def k_means(self):   
         #step 1: initialize the centroids k times that are from the data points
-        self.centroids = self.initialize_centroids()
+        centroids_list = self.centroids
         
         for n in range(self.max_iter):    
-        
-        # step 2: for each data point in the data set, assign a cluster that is closest to the data point
-        
-        # helper function to assign clusters to data points
-            for data_point in self.data:
+            
+            # assign the clusters to the data points, based on the closest centroid
+            # Randomly assign each observation to an initial cluster, from 1 to K. 
+            clusters = self.assign_clusters(centroids_list, self.data)
+            # update the centroids
+            centroids_list = self.update_centroids(clusters, self.data)
+            # update the objective function
+            self.inertia.append(self.objective_function(clusters, centroids_list, self.data))
                 
-                clusters = np.argmin(np.linalg.norm(data_point[2:4] - self.centroids[:, 2:4]))
-                self.cluster_assignment = clusters
-                
-        # helper function to update the centroids
-                  
-            # step 3: update the centroids by calculating the mean of the data points in the cluster 
-            for i in range(self.k):
-                if len(self.data[self.cluster_assignment == i]) != 0:
-                    self.centroids[i] = np.mean(self.data[self.cluster_assignment == i])
-                    
-        # helper function that calculates the objective function to make sure it is decreasing with new centroids.  
-  
-        # step 4: repeat step 2 and 3 until the centroids do not change or max iterations reached
-        
         # step 5: return the centroids and cluster assignments
-        return self.centroids, self.cluster_assignment 
+        return centroids_list
+    
+    def plot_objective_function(self):
+        # plot the objective function values, make sure they minimize everytime
+        plt.plot(self.inertia)
         
-    # method to plot the data points and centroids as they change
-    
-    
+        plt.xlabel("iteration")
+        plt.ylabel("Sum of error")
+        
+        plt.show()
+        
+        
+        
     #plot the data points and centroids
     def plot_data_points(self):     
         # plot the based on different colors, flower type  
@@ -100,39 +115,45 @@ class K_Means():
             
         plt.show()
         
-    # make sure objective function is decreasing and is following the elbow method
-    def plot_objective_function(self) -> None:    
-        objective_vals = []
-        
-        print(self.kmeans())
-        
-        for i in range(1,11):
-           
-            objective_vals.append(self.objective_function())
-        
-        
-        plt.plot(objective_vals)
-        plt.xlabel('Iterations')
-        plt.ylabel('squared error sum')
-        
-        plt.show()
               
 if __name__ == "__main__":
-     
+    
+    
+    
+    x = K_Means(2, data, 60)
+    
+    x.plot_objective_function()
+    
+    
+      
+    '''
     iris = pd.read_csv("irisdata.csv")
 
-    features = ['sepal_length','sepal_width','petal_length', 'petal_width', 'species']
+    features = ['sepal_length','sepal_width','petal_length', 'petal_width']
 
     data_vector = iris[features].values
     
-    x = K_Means(2, data_vector, 200)
-    
-    #x.plot_data_points()
+    x = K_Means(2, data_vector, 70)
     
     
-    y = K_Means(2, data_vector, 70)
+    x.k_means()
     
-    y.plot_objective_function()
+    
+    x.plot_objective_function()
+    '''
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
